47bf2fe8f15176243b97676c2f3db776bbeabd94
Ian Rees <ian@ianrees.net>
2024-10-09 18:16:37 -0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+    ssr: false,
+    compatibilityDate: "2024-10-09",
+    plugins: ["./oruga.ts"],
+    css: ['./main.scss']
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>This is an <a href="/">Example Page</a></p>
+    <br>
+    <o-notification variant="primary">This is a test bulma component.</o-notification>
+  </div>
+</template>
